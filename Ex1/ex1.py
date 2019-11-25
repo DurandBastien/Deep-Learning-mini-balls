f514dc4c2694618c9a2ec1d396853faa0ceb3dae
ClementGuittat <32964240+ClementGuittat@users.noreply.github.com>
2019-11-25 17:00:21 +0100

    avancée code

--- Ex1/ex1.py
+++ Ex1/ex1.py
@@ -1,6 +1,9 @@
+import torch
+from torch.nn import functional as F
 import tp_src.dataset_det as d_loader
 import torch.utils.data as torch_d
 
+STATS_INTERVAL = 200
 
 class LeNet(torch.nn.Module):
     def __init__(self):
@@ -20,42 +23,110 @@ class LeNet(torch.nn.Module):
         x = self.fc2(x)
         return x
 
+def transform(img):
+    return img.flatten()
+
+# ************************************************************************
+# Calculate the error of a model on data from a given loader
+# This is used to calculate the validation error every couple of
+# thousand batches
+# ************************************************************************
+
+def calcError (net, dataloader):
+    vloss=0
+    vcorrect=0
+    vcount=0
+    for batch_idx, (data, labels) in enumerate(dataloader):
+        y = model(data)
+        loss = crossentropy(y, labels)
+        vloss += loss.item()
+        _, predicted = torch.max(y.data, 1)
+        vcorrect += (predicted == labels).sum().item()
+        vcount += BATCHSIZE
+    return vloss/len(dataloader), 100.0*(1.0-vcorrect/vcount)
+
 if __name__ == "__main__":
     # train_dataset = Balls_CF_Detection ("../mini_balls/train", 20999,
     #     transforms.Normalize([128, 128, 128], [50, 50, 50]))
-    dataset = d_loader.Balls_CF_Detection ("train", 20999)
+    dataset = d_loader.Balls_CF_Detection ("train", 20999, transform)
 
-    train_dataset, test_dataset = torch_d.random_split(dataset, [20999*0.8, 20999*0.2]) 
+    train_dataset, test_dataset = torch_d.random_split(dataset, [16799, 4200]) 
+    
+    BATCHSIZE=50
+    
+    test_loader = torch.utils.data.DataLoader(test_dataset,
+        batch_size=BATCHSIZE, shuffle=True)
+    
+    train_loader = torch.utils.data.DataLoader(train_dataset,
+        batch_size=BATCHSIZE, shuffle=True)
+    
+    img, labels, bb = train_dataset.__getitem__(42)
+    print(img.size())
+    
+    model = LeNet()
 
-    print(len(train_dataset))
- #    BATCHSIZE=50
+    # This criterion combines LogSoftMax and NLLLoss in one single class.
+    crossentropy = torch.nn.CrossEntropyLoss(reduction='mean')
+    
+    # Set up the optimizer: stochastic gradient descent
+    # with a learning rate of 0.01
+    optimizer = torch.optim.SGD(model.parameters(), lr=0.01)
+    
+    # Setting up tensorboard
+    #writer = SummaryWriter('runs/mnist_lenet')
+    
+    # Training
+    running_loss = 0.0
+    running_correct = 0
+    running_count = 0
+    
+    # Add the graph to tensorboard
+    #dataiter = iter(train_loader)
+    #data, labels = dataiter.next()
+    #writer.add_graph (model, data)
+    #writer.flush()
+    
+    # Cycle through epochs
+    for epoch in range(1):
+        
+        # Cycle through batches
+        for batch_idx, (data, labels, bb) in enumerate(train_loader):
+            
+            labels = labels.type(torch.LongTensor)
+            print(labels)
+            optimizer.zero_grad()
+            y = model(data)
+            
+            loss = crossentropy(y, labels)
+            loss.backward()
+            running_loss += loss.item()
+            optimizer.step()
+    
+            _, predicted = torch.max(y.data, 1)
+            running_correct += (predicted == labels).sum().item()
+            running_count += BATCHSIZE
+    
+    		# Print statistics
+            if (batch_idx % STATS_INTERVAL) == 0:
+                train_err = 100.0*(1.0-running_correct / running_count)
+                valid_loss, valid_err = calcError (model, test_loader)
+                print ('Epoch: %d batch: %5d ' % (epoch + 1, batch_idx + 1), end="")
+                print ('train-loss: %.3f train-err: %.3f' % (running_loss / STATS_INTERVAL, train_err), end="")
+                print (' valid-loss: %.3f valid-err: %.3f' % (valid_loss, valid_err))
+    
+                # Write statistics to the log file
+                #writer.add_scalars ('Loss', {
+                    #'training:': running_loss / STATS_INTERVAL,
+                    #'validation:': valid_loss }, 
+                   # epoch * len(train_loader) + batch_idx)
+    
+               # writer.add_scalars ('Error', {
+                    #'training:': train_err,
+                    #'validation:': valid_err }, 
+                    #epoch * len(train_loader) + batch_idx)
+                                
+                running_loss = 0.0
+                running_correct = 0.0
+                running_count=0.0
 
 
-
-	# valid_dataset = MNISTDataset ("MNIST-png/testing", 
- #    transforms.Compose([
- #    transforms.ToTensor(),
- #    transforms.Normalize((0.1307,), (0.3081,))])) # mean, std of dataset
-	# valid_loader = torch.utils.data.DataLoader(valid_dataset,
- #    batch_size=BATCHSIZE, shuffle=True)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- #    img,p,b = train_dataset.__getitem__(42)
-
- #    print ("Presence:")
- #    print (p)
-
- #    print ("Pose:")
- #    print (b)
